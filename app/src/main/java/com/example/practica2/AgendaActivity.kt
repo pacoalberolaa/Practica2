1dc682d9c3cdea4a52651d6cd2a662b2ed113533
Pacoalberola <falberola2003@gmail.com>
2022-11-08 20:04:06 +0100

    Commit final parte 1

--- app/src/main/java/com/example/practica2/AgendaActivity.kt
+++ app/src/main/java/com/example/practica2/AgendaActivity.kt
@@ -1,11 +1,50 @@
 package com.example.practica2
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 
 class AgendaActivity : AppCompatActivity() {
+
+    private lateinit var edNuevoContacto:TextView
+    private lateinit var edNombre:EditText;
+    private lateinit var edApellido:EditText;
+    private lateinit var edTelefono:EditText;
+    private lateinit var tvNotas:TextView
+    private lateinit var btAceptar: Button;
+    private lateinit var btCancelar:Button;
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_agenda)
+
+        edNuevoContacto=findViewById(R.id.tvNuevoContacto)
+        edNombre=findViewById(R.id.edNombre)
+        edApellido=findViewById(R.id.edApellidos)
+        edTelefono=findViewById(R.id.edTelefono)
+        tvNotas=findViewById(R.id.tvNotas)
+        btAceptar=findViewById(R.id.btAceptar)
+        btCancelar=findViewById(R.id.btCancelar)
+
+        btCancelar.setOnClickListener{finish()}
+        btAceptar.setOnClickListener { cambiarNombre() }
+
     }
+
+    fun cambiarNombre(){
+        if ((!edNombre.text.toString().isEmpty()) or (!edApellido.text.toString().isEmpty())){
+            edNuevoContacto.text = String.format(edNombre.text.toString() + " " + edApellido.text.toString())
+        }else{
+            Toast.makeText(this, getString(R.string.msj_nombre), Toast.LENGTH_LONG).show()
+
+        }
+
+    }
+
+
 }
--- app/src/main/java/com/example/practica2/MainActivity.kt
+++ app/src/main/java/com/example/practica2/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.example.practica2
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var tvNombre: TextView;
+    private lateinit var btAgenda: Button;
+    private lateinit var btReceta:Button;
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tvNombre=findViewById(R.id.tvNombre)
+        btAgenda=findViewById(R.id.btAgenda)
+        btReceta=findViewById(R.id.btReceta)
+
+        btAgenda.setOnClickListener { enviarAgenda() }
+
+    }
+
+    fun enviarAgenda(){
+        val intent = Intent(this, AgendaActivity::class.java)
+        startActivity(intent)
+
     }
 }
