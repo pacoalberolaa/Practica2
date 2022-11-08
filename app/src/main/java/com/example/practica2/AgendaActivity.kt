cbbc95a2059af05ef7b1a925ba9100e41129d23c
Pacoalberola <falberola2003@gmail.com>
2022-11-08 19:15:28 +0100

    Commit paso 23

---
+++ app/src/main/java/com/example/practica2/AgendaActivity.kt
@@ -0,0 +1,11 @@
+package com.example.practica2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AgendaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_agenda)
+    }
+}
